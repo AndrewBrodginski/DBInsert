e2cdd91d9360c583e5da1c6f6397e7803e48d304
AndrewBrodginski <AndrewBrodginski@users.noreply.github.com>
2016-11-06 20:01:46 -0500

    Update Controller.java

--- src/com/shanemalachow/DBInserter/Controller.java
+++ src/com/shanemalachow/DBInserter/Controller.java
@@ -10,10 +10,29 @@ public class Controller {
 	private JFrame frame;
 	private DBInterface db;
 
+	/**
+	 * Standard constructor for the Controller, creating the frame work of GUI
+	 */
+	
 	public Controller() {
 		frame = new MainGUI(this);
 		db = null;
 	}
+	
+	/**
+	 * Determines where or not the data was Inserted with a true or false
+	 * 
+	 * @param table
+	 * 		The table the data is being inserted into
+	 * @param file
+	 * 		The name of the file to be read
+	 * @param delim
+	 * 		The delimiter between attributes of each entry.
+	 * @return
+	 *		Whether or not the data was inserted 
+	 *
+	 */
+
 
 	public boolean insertData(String table, String file, String delim) {
 		FileParser in;
@@ -39,6 +58,19 @@ public class Controller {
 			return false;
 		}
 	}
+	
+	/**
+	 * Determines whether or not the User logged in successfully
+	 * 
+	 * @param db
+	 * 		The database the user is trying to access
+	 * @param user
+	 * 		The login ID for the user
+	 * @param pass
+	 * 		The users password for the database
+	 * @return
+	 * 		A true or false, indicating if the login was successful
+	 */
 
 	public boolean login(String db, String user, char[] pass) {
 		try {
